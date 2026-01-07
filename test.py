e3f0f735eb2fb158cf203d5245c755856ce78575
Rodney Lei Estrada <rodneyestrada2425@gmail.com>
2026-01-07 17:55:32 +0800

    .

--- test.py
+++ test.py
@@ -1,12 +1,73 @@
 import marimo
 
 __generated_with = "0.18.4"
-app = marimo.App(width="medium")
+app = marimo.App(width="medium", layout_file="layouts/test.slides.json")
 
 
 @app.cell
 def _():
-    print("pogi")
+    import marimo as mo
+
+    mo.md("# Welcome to marimo!🥀")
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    text_input = "rodney"
+    mo.md(f"Enter some text: {text_input}")
+    return
+
+
+@app.cell
+def _():
+    a = 3 
+    b = 4
+    print(a + b)
+    return
+
+
+@app.cell
+def _():
+    _a = 5
+    return
+
+
+@app.cell
+def _():
+    # lazy mode
+    y = 36
+    z = 20
+    return y, z
+
+
+@app.cell
+def _(y, z):
+    y + z
+    return
+
+
+@app.cell
+def _(mo):
+    icon = mo.ui.dropdown(["🥰", "😎", "🤖"], value="😎")
+    return (icon,)
+
+
+@app.cell
+def _(icon, mo):
+    repetitions = mo.ui.slider(1, 16, label=f"number of {icon.value}:")
+    return (repetitions,)
+
+
+@app.cell
+def _(icon, repetitions):
+    icon, repetitions
+    return
+
+
+@app.cell
+def _(icon, mo, repetitions):
+    mo.md("# " + icon.value * repetitions.value)
     return
 
 
